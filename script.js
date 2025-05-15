dd097ddd932123355d059cacb29f9d924c759ae9
Victor Seleimend <sseleimend@gmail.com>
2025-05-15 08:34:01 -0300

    Change the arrow function to a function declaration

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ const calculatorDisplay = document.querySelector("h1");
 const inputBtns = document.querySelectorAll("button");
 const clearBtn = document.getElementById("clear-btn");
 
+let firstValue = 0;
+
+function addNumberValue(number) {
+  console.log(number);
+}
+
 inputBtns.forEach((inputBtn) => {
   if (inputBtn.classList.length === 0) {
-    inputBtn.addEventListener("click", () => {
-      console.log(inputBtn.value);
-    });
+    inputBtn.addEventListener("click", () => addNumberValue(inputBtn.value));
   }
 });
